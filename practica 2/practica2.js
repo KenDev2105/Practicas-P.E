687e163acc3bdebbd4e990362fcbcec9466a96fa
Your Name <195981080+KenDev2105@users.noreply.github.com>
2025-08-27 14:27:49 -0600

    Ended practica2.js

--- practica 2/practica2.js
+++ practica 2/practica2.js
@@ -1,3 +1,4 @@
+import { Console } from 'console';
 import readline from 'readline'
 
 const rl = readline.createInterface({
@@ -5,35 +6,43 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-rl.question("Escribe una operación ejemplo: 10 + 20, 7.5 * 3, 100 / 0 \n", (operacion) => {
-    const toArray = operacion.split(' ')
-    const num1 = parseFloat(toArray[0]),
-        num2 = parseFloat(toArray[2]),
-        operador = toArray[1]
+// rl.question("Escribe una operación ejemplo: 10 + 20, 7.5 * 3, 100 / 0 \n", (operacion) => {
+//     const toArray = operacion.split(' ')
+//     const num1 = parseFloat(toArray[0]),
+//         num2 = parseFloat(toArray[2]),
+//         operador = toArray[1]
 
-        let resultado;
+//         let resultado;
 
-    switch (operador) {
-        case '+':
-            resultado = num1 + num2
-            console.log(`${num1} ${operador} ${num2} = ${res}`);
-            break;
-        case '-':
-            resultado = num1 - num2
-            console.log(`${num1} ${operador} ${num2} = ${res}`);
-            break;
-        case '*':
-            resultado = num1 * num2
-            console.log(`${num1} ${operador} ${num2} = ${res}`);
-            break;
-        case '/':
-            resultado = num1 / num2
-            console.log(`${num1} ${operador} ${num2} = ${res}`);
-            break;
-        default:
-        console.error('Operación no valida')
-            break;
-    }
-    rl.close()
+//     switch (operador) {
+//         case '+':
+//             resultado = num1 + num2
+//             console.log(`${num1} ${operador} ${num2} = ${res}`);
+//             break;
+//         case '-':
+//             resultado = num1 - num2
+//             console.log(`${num1} ${operador} ${num2} = ${res}`);
+//             break;
+//         case '*':
+//             resultado = num1 * num2
+//             console.log(`${num1} ${operador} ${num2} = ${res}`);
+//             break;
+//         case '/':
+//             resultado = num1 / num2
+//             console.log(`${num1} ${operador} ${num2} = ${res}`);
+//             break;
+//         default:
+//         console.error('Operación no valida')
+//             break;
+//     }
+//     rl.close()
+// })
+
+// Pide al usuario que escriba un número decimal (Ejemplo: 7.35)
+rl.question("Escribe un número decimal: ", (decimal) => {
+    console.log("Math Floor: " , Math.floor(decimal))
+    console.log("Math Round", Math.round(decimal))
+    console.log("Math Ceil", Math.ceil(decimal))
+    console.log("Math Trunc", Math.trunc(decimal))
+ rl.close()
 })
-
